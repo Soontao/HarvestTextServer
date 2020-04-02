20850a354609ff3694d78963674e1f847da31530
Theo Sun <theo.sun@sap.com>
2020-04-02 16:12:41 +0800

    feat: language detect

--- nlp.py
+++ nlp.py
@@ -1,14 +1,51 @@
 from flask import Blueprint, request
 from jiagu import sentiment
 from error import ParameterLostError
+from textblob import TextBlob
+from langid import classify
 
 nlp_api = Blueprint("npl_api", __name__)
 
 
-@nlp_api.route("/sent")
-def text_sent():
+@nlp_api.route("/lang_detect")
+def lang_detect():
     text = request.args.get("text")
     if text is None:
+        raise ParameterLostError("text")
+    r = classify(text)
+    return {
+        "lang": r[0],
+        "probability": r[1]
+    }
+
+
+@nlp_api.route("/sentiment_zh")
+def text_sentiment_zh():
+    text = request.args.get("text")
+
+    if text is None:
         raise ParameterLostError("sentiment_text")
+
     result = sentiment(text)
-    return {"sentiment": result[0], "probability": result[1], "code": 200}
+
+    return {
+        "code": 200,
+        "sentiment": result[0],
+        "probability": result[1]
+    }
+
+
+@nlp_api.route("/sentiment_en")
+def text_sentiment_en():
+    text = request.args.get("text")
+
+    if text is None:
+        raise ParameterLostError("sentiment_text")
+
+    tb = TextBlob(text)
+
+    return {
+        "code": 200,
+        "polarity": tb.sentiment.polarity,
+        "subjectivity": tb.sentiment.subjectivity
+    }
